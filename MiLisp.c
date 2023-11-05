28ca8b410d0b3cfcfc0f84f27423374dd8871a76
Yeelght <91862060+FeiNiaoBF@users.noreply.github.com>
2023-11-05 14:44:38 +0800

    V--0.0.2.6 add cmp if

--- MiLisp.c
+++ MiLisp.c
@@ -122,6 +122,18 @@ lval* builtin_join(lenv* e, lval* a);
 lval* builtin_len(lenv* e, lval* a);
 lval* builtin_cons(lenv* e, lval* a);
 lval* builtin_init(lenv* e, lval* a);
+// 分支
+lval* builtin_ord(lenv* e, lval* a, char* op);
+lval* builtin_cmp(lenv* e, lval* a, char* op);
+lval* builtin_gt(lenv* e, lval* a);
+lval* builtin_lt(lenv* e, lval* a);
+lval* builtin_ge(lenv* e, lval* a);
+lval* builtin_le(lenv* e, lval* a);
+lval* builtin_eq(lenv* e, lval* a);
+lval* builtin_ne(lenv* e, lval* a);
+// if 函数
+lval* builtin_if(lenv* e, lval* a);
+
 //
 lval* builtin_var(lenv* e, lval* a, char* func);
 lval* builtin_def(lenv* e, lval* a);
@@ -138,7 +150,7 @@ lval* lval_sexpr(void);
 void  lval_del(lval* v);
 lval* lval_copy(lval* v);
 lval* lval_call(lenv* e, lval* f, lval* a);
-
+int lval_eq(lval* x, lval* y);
 // create qexpr type
 lval* lval_qexpr(void);
 
@@ -189,7 +201,7 @@ mpc_parser_t* Lispy    = mpc_new("lispy");
     Number, Symbol, Sexpr, Qexpr, Expr, Lispy);
 
 
-puts("MiLisp Version 0.0.2.5");
+puts("MiLisp Version 0.0.2.6");
 puts("Press <Ctrl+c> to Exit\n");
 
 lenv* e = lenv_new();
@@ -736,6 +748,14 @@ void lenv_add_builtins(lenv* e) {
   lenv_add_builtin(e, "=",   builtin_put);
   // Lambda Function
   lenv_add_builtin(e, "\\", builtin_lambda);
+  // Comparison Function
+  lenv_add_builtin(e, "if", builtin_if);
+  lenv_add_builtin(e, "==", builtin_eq);
+  lenv_add_builtin(e, "!=", builtin_ne);
+  lenv_add_builtin(e, ">",  builtin_gt);
+  lenv_add_builtin(e, "<",  builtin_lt);
+  lenv_add_builtin(e, ">=", builtin_ge);
+  lenv_add_builtin(e, "<=", builtin_le);
 }
 
 
@@ -870,3 +890,110 @@ lval* lval_call(lenv* e, lval* f, lval* a) {
     return lval_copy(f);
   }
 }
+
+// 排序函数
+lval* builtin_ord(lenv* e, lval* a, char* op){
+  LASSERT_NUM(op, a, 2);
+  LASSERT_TYPE(op, a, 0, LVAL_NUM);
+  LASSERT_TYPE(op, a, 1, LVAL_NUM);
+
+  int r;
+  if (strcmp(op, ">") == 0)  {
+    r = (a->cell[0]->lnum > a->cell[1]->lnum);
+  }
+  if (strcmp(op, "<") == 0)  {
+    r = (a->cell[0]->lnum < a->cell[1]->lnum);
+  }
+  if (strcmp(op, ">=") == 0)  {
+    r = (a->cell[0]->lnum >= a->cell[1]->lnum);
+  }
+  if (strcmp(op, "<=") == 0)  {
+    r = (a->cell[0]->lnum <= a->cell[1]->lnum);
+  }
+  lval_del(a);
+  return lval_num(r);
+}
+
+lval* builtin_gt(lenv* e, lval* a){
+  return builtin_ord(e, a, ">");
+}
+lval* builtin_lt(lenv* e, lval* a){
+  return builtin_ord(e, a, "<");
+}
+lval* builtin_ge(lenv* e, lval* a){
+  return builtin_ord(e, a, ">=");
+}
+lval* builtin_le(lenv* e, lval* a){
+  return builtin_ord(e, a, "<=");
+}
+
+int lval_eq(lval* x, lval* y) {
+  if (x->lisptype != y->lisptype) {return 0;}
+
+  switch (x->lisptype)
+  {
+  case LVAL_NUM:
+    return (x->lnum == y->lnum);
+  case LVAL_ERR:
+    return (strcmp(x->err, y->err) == 0);
+  case LVAL_SYM:
+    return (strcmp(x->sym, y->sym) == 0);
+  case LVAL_FUN:
+    if (x->builtin || y->builtin) {
+      return x->builtin == y->builtin;
+    } else {
+      return lval_eq(x->formals, y->formals) 
+              && lval_eq(x->body, y->body);
+    }
+  case LVAL_QEXPR:
+  case LVAL_SEXPR:
+    if (x->count != y->count) {return 0;}
+    for (int i = 0; i < x->count; i++) {
+      if (!lval_eq(x->cell[i], y->cell[i])) {return 0;}
+    }
+    return 1;
+  break;
+  }
+  return 0;
+}
+
+lval* builtin_cmp(lenv* e, lval* a, char* op) {
+  LASSERT_NUM(op, a, 2);
+  int r;
+  if (strcmp(op, "==") == 0) {
+    r = lval_eq(a->cell[0], a->cell[1]);
+  }
+  if (strcmp(op, "!=") == 0) {
+    r = !lval_eq(a->cell[0], a->cell[1]);
+  }
+  lval_del(a);
+  return lval_num(r);
+}
+
+lval* builtin_eq(lenv* e, lval* a){
+  return builtin_cmp(e, a, "==");
+}
+lval* builtin_ne(lenv* e, lval* a){
+  return builtin_cmp(e, a, "!=");
+}
+
+lval* builtin_if(lenv* e, lval* a) {
+  LASSERT_NUM("if", a, 3);
+  LASSERT_TYPE("if", a, 0, LVAL_NUM);
+  LASSERT_TYPE("if", a, 1, LVAL_QEXPR);
+  LASSERT_TYPE("if", a, 2, LVAL_QEXPR);
+
+  lval* x;
+  a->cell[1]->lisptype = LVAL_SEXPR;
+  a->cell[2]->lisptype = LVAL_SEXPR;
+
+  if (a->cell[0]->lnum) {
+    x = lval_eval(e, lval_pop(a, 1));
+  } else {
+    x = lval_eval(e, lval_pop(a, 2));
+  }
+  lval_del(a);
+  return x;
+}
+
+
